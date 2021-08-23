ba237431ed4967be249be0d13932fa924216e486
Lucas Galdino <lucasgaldino7@gmail.com>
2021-08-23 11:20:20 -0300

    day 1

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Botao from "./components/Botao.svelte";
+</script>
+
+<main>
+  <Botao texto="7" />
+  <Botao texto="8" />
+  <Botao texto="9" />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/components/Botao.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let texto: string;
+</script>
+
+<button class="botao"> {texto} </button>
+
+<style>
+  .botao {
+  }
+</style>
